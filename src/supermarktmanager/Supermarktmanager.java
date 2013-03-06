f147f917180cf1f12db978875f6cebe8a8513eee
TheImpatient <benjvdv@gmail.com>
2013-03-06 17:03:48 +0100

    First steps

--- src/supermarktmanager/Supermarktmanager.java
+++ src/supermarktmanager/Supermarktmanager.java
@@ -1,19 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package supermarktmanager;
 
-/**
- *
- * @author Ben
- */
 public class Supermarktmanager {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
 }
