1d26b42a115313671589f93a0d9958522bc9d832
TheImpatient <benjvdv@gmail.com>
2013-03-13 18:53:02 +0100

    Added interface persoon

--- src/supermarktmanager/ProductHistory.java
+++ src/supermarktmanager/ProductHistory.java
@@ -1,11 +1,65 @@
 package supermarktmanager;
 
 import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
 
+@Entity
 public class ProductHistory {
-    int id;
-    int productId;
-    int aantal = 0;
-    Date dateBegin;
-    Date dateEind;
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private int productId;
+    
+    private int aantal = 0;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateBegin = new Date();
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateEind;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getAantal() {
+        return aantal;
+    }
+
+    public void setAantal(int aantal) {
+        this.aantal = aantal;
+    }
+
+    public Date getDateBegin() {
+        return dateBegin;
+    }
+
+    public void setDateBegin(Date dateBegin) {
+        this.dateBegin = dateBegin;
+    }
+
+    public Date getDateEind() {
+        return dateEind;
+    }
+
+    public void setDateEind(Date dateEind) {
+        this.dateEind = dateEind;
+    }
+    
+    
 }
