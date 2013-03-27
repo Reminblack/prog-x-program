33c8473d50781e98baec7ce8c0c5d14001169f96
Bart de Ruijter <Bulldog_233@hotmail.com>
2013-03-27 11:45:24 -0600

    Commit

---
+++ src/supermarktmanager/Persoon.java
@@ -0,0 +1,17 @@
+package supermarktmanager;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy= InheritanceType.SINGLE_TABLE)
+public abstract class Persoon {
+    @Id @GeneratedValue
+    private Long id;
+    
+    private String name;
+
+}
