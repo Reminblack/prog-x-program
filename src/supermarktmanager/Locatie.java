ebd3c9162529c143a8a29de9450bc163fb74077a
Reminblack <Marinus.C.Visser@gmail.com>
2013-04-22 16:59:27 +0200

    AAAAAAAAAAAAAAAAAAAAAARRRRGHHHHHHHHHHHHHHHAAAAAAAAAAAAARGGGHGHGHGHHHHHH 1.2.3.1

--- src/supermarktmanager/Locatie.java
+++ src/supermarktmanager/Locatie.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Locatie {
@@ -14,7 +15,26 @@ public class Locatie {
     private Long id;
     
         @ManyToMany
-    protected List<Persoon> werkpersonen = new ArrayList<Persoon>();
+    private List<Persoon> werkPersonen = new ArrayList<Persoon>();
+        
+        @ManyToOne
+        private List<Container> containers = new ArrayList<Container>();
+
+    public List<Persoon> getWerkpersonen() {
+        return werkPersonen;
+    }
+
+    public void setWerkpersonen(List<Persoon> werkpersonen) {
+        this.werkPersonen = werkpersonen;
+    }
+
+    public List<Container> getContainers() {
+        return containers;
+    }
+
+    public void setContainers(List<Container> containers) {
+        this.containers = containers;
+    }
 
     public Long getId() {
         return id;
@@ -22,5 +42,25 @@ public class Locatie {
 
     public void setId(Long id) {
         this.id = id;
+    }
+    
+    public void addContainer(Container c)
+    {
+        containers.add(c);
+    }
+    
+    public void removeContainer(Container c)
+    {
+        containers.remove(c);
+    }
+    
+    public void addWerknemer(Persoon p)
+    {
+        werkPersonen.add(p);
+    }
+    
+    public void removeWerknemer(Persoon p)
+    {
+        werkPersonen.remove(p);
     }
 }
