32c06ac251143445e85688f1ce7307928695460a
Reminblack <Marinus.C.Visser@gmail.com>
2013-04-22 15:27:37 +0200

    synch commit 1.2.2.1

---
+++ src/ServiceLayer/StaticContainer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServiceContainer;
+
+import DAOClasses.*;
+import java.util.HashMap;
+import org.hibernate.Session;
+import supermarktmanager.newHibernateUtil;
+/**
+ *
+ * @author Bart
+ */
+public class StaticContainer {
+    
+   private static StaticContainer instance = null;
+   private static HashMap<String, Object> services = new HashMap();
+   private Session hibernateSession;
+   
+   protected StaticContainer(){
+       hibernateSession = newHibernateUtil.getSessionFactory().getCurrentSession();
+       
+       VakkenVullerService vvs = new VakkenVullerService();
+       vvs.setVakkenVullerDao(new VakkenVullerDao());
+       services.put("VakkenVullerService", vvs);
+       
+       LocatieService ls = new LocatieService();
+       ls.setLocatieDao(new LocatieDao());
+       services.put("LocatieService", ls);
+   }
+   
+   public static StaticContainer getInstance() {
+      if(instance == null) {
+         instance = new StaticContainer();
+      }
+      return instance;
+   }
+   
+   public static Object getService(String service_id){
+       return services.get(service_id);
+   }
+   
+   public static Session getSession(){
+       if(!instance.hibernateSession.isOpen()){
+           instance.hibernateSession = newHibernateUtil.getSessionFactory().getCurrentSession();
+       }
+       return instance.hibernateSession;
+       
+   }
+   
+}
